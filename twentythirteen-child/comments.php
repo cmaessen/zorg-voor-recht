0d3e8d797ed7c63cd89a0fcc1c5d54834b094802
collinmaessen <c.maessen@mijnonlineplekje.nl>
2016-05-08 09:52:58 +0200

    first commit

---
+++ twentythirteen-child/comments.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template for displaying Comments
+ *
+ * The area of the page that contains comments and the comment form.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
